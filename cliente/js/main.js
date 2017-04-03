a27b887464ebb1bf900488f4a85c011a24858042
jesser pariona <jerspr@gmail.com>
2017-04-02 22:08:57 -0500

    üpdate

--- cliente/js/main.js
+++ cliente/js/main.js
@@ -1,6 +1,26 @@
 var obj = {
 	init: function(){
-		console.log("Prueba");
+		if($("#filterS").length>0){
+			console.log("dentrando");
+			obj.filterSearch();
+		}
+	},
+	filterSearch: function(){
+		$('#fCodigo').multiselect({
+            includeSelectAllOption: true,
+            nonSelectedText: 'Código',
+            buttonWidth: '108px'
+        });
+        $('#cCNO').multiselect({
+            includeSelectAllOption: true,
+            nonSelectedText: 'Código CNO',
+            buttonWidth: '126px'
+        });
+        $('#cConsultora').multiselect({
+            includeSelectAllOption: true,
+            nonSelectedText: 'Código Consultora',
+            buttonWidth: '178px'
+        });
 	}
 };
 (function() {
