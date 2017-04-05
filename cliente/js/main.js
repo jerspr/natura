70f6d5890ab16e53713c53fa80f96807cfc9c470
jesser pariona <jerspr@gmail.com>
2017-04-05 00:52:26 -0500

    üpdate

--- cliente/js/main.js
+++ cliente/js/main.js
@@ -1,9 +1,11 @@
 var obj = {
 	init: function(){
 		if($("#filterS").length>0){
-			console.log("dentrando");
 			obj.filterSearch();
 		}
+        if($("#tableCN").length>0){
+            obj.nTable();
+        }
 	},
 	filterSearch: function(){
 		$('#fCodigo').multiselect({
@@ -21,7 +23,49 @@ var obj = {
             nonSelectedText: 'Código Consultora',
             buttonWidth: '178px'
         });
-	}
+	},
+    nTable: function(){
+        $('.collapse').on('show.bs.collapse', function () {
+            $('.collapse.in').collapse('hide');
+        });
+        $('.popup-gallery').magnificPopup({
+            delegate: 'a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function(item) {
+                    return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+                }
+            }
+        });
+    },
+    nDonut: function(){
+        /*new Chartist.Pie('.ct-chart', {
+            labels: ['Piece A', 'Piece B', 'Piece C', 'Piece D'],
+            series: [75, 5, 8, 15]
+        }, {
+            height: '200px',
+            donut: true,
+            donutWidth: 30,
+            startAngle: 270,
+            total: 100,
+            showLabel: true,
+            labelInterpolationFnc: function(value) {
+                return value + '%';
+            },
+            plugins: [
+                Chartist.plugins.legend()
+            ]
+        });*/
+        
+    }
 };
 (function() {
   	obj.init();
